1612a5feecbd5130f7ca23123e6fd0f04d1f7f57
jm-rives <joanna.rives@gmail.com>
2016-08-25 10:55:35 -0700

    ADD Find method

--- bank_account.rb
+++ bank_account.rb
@@ -5,12 +5,17 @@
 #
 # Primary Requirements
 #
-# [] Update the Account class to be able to manage fields from the CSV file used as input.
-# 	 For example, manually choose the data from the first line of the CSV file and ensure you can create a new instance of your Account using that data
+# [x] Update the Account class to be able to manage fields from the CSV file used as input.
+# 	 	For example, manually choose the data from the first line of the CSV file
+#   	and ensure you can create a new instance of your Account using that data
+#
 # Add the following class methods to your existing Account class
-# [ ] self.all - returns a collection (array) of Account instances, representing all of the Accounts described in the CSV. See below for the CSV file specifications
-# [ ] self.find(id) - returns an instance of Account where the value of the id field in the CSV matches the passed parameter
-# [ ] CSV Data File
+#
+# [x ] self.all - returns a collection (array) of Account instances, r
+#     representing all of the Accounts described in the CSV. See below for the CSV file specifications
+# [ x] self.find(id) - returns an instance of Account where the value 
+#    of the id field in the CSV matches the passed parameter
+# [ x] CSV Data File
 
 # Bank::Account
 
@@ -20,29 +25,31 @@
 # OpenDate - (Datetime) when the account was opened
 
 # () What tests should I write for a module, classess within it, methods?
-
 require 'csv'
-# require_relative './support/accounts.csv' #why you no werk require, why?
+raw_acct_data = CSV.read('./support/accounts.csv')
 
-module Bank
-	
+
+
+
+
+module Bank	
 	class Account
-		# ruby dosen't like constants in intialize values, lowercase it
-		attr_reader :id, :open_date
+
+		attr_reader :id, :open_date # read only as these should be immutable
 		attr_accessor :balance # this should permit reading and writing
-		#accounts = 
-		CSV.read("./support/accounts.csv")	
+
 		# intial_balance in pennies
-		#returns a collection (array) of Account instances, at least it's supposed to. 
-		
-		def initialize(id, balance)
+
+		def initialize(id, balance, open_date)
+			# ruby dosen't like constants in intialize values, lowercase it
 			@id = id
 			@balance = balance
 			@open_date = open_date
-		end
+			# pretty_initial_balance = (@initial_balance / 100).to_f
 			is_negative # method to determine if transaction would lead to overdraft
+		end
 
-		# print debuggin' # raises error as expected if transaction is negative		
+# print debuggin' # raises error as expected if transaction is negative		
 		def is_negative
 			unless @balance > 0 
 				raise ArgumentError.new("You must enter a positive balance!")
@@ -65,20 +72,76 @@ module Bank
 			see_balance
 		end
 
-	self.all # class method
-			all_accounts = []
-			CSV.foreach("path/to/file.csv") do |row|
-				all_accounts.push(row)
-				return all_accounts
+		def self.all(csv_data)
+			accounts_all = []
+			csv_data.each do |line|
+				#puts line werks as expected
+				line_acct_id = line[0] 
+				#puts line_acct_id
+				current_balance = line[1]
+				# puts current_balance
+				open_date = line[2]
+				#puts open_date
+
+				new_account = Bank::Account.new(line_acct_id, current_balance.to_f, open_date)
+				accounts_all.push(new_account)
 			end
-			
-	self.find(id) #class method
-	return find
-	end	
+			return  accounts_all
+			#return csv_data
+		end
+
+		#FROM Austin, this method should take an array of accounts as a parameter, in addition the ID
+		def self.find(id, accounts)
+			accounts.each do |account|
+
+				if id == account.id
+					return account
+
+
+				end
+			end
+			return nil #"Account not found." # generally bad for fxns to return two dif kinds of data, here you may get acct or string. Nil might be better.
+		end
+	end
 end
 
-acct_1 = Bank::Account.new
-puts acct_1
+
+#TEST 5
+accounts = Bank::Account.all(raw_acct_data)
+puts Bank::Account.find('15155', accounts)
+
+#TEST 4
+# accounts = Bank::Account.all(raw_acct_data)
+# puts Bank::Account.find('1213', accounts)
+
+#TEST 3
+# puts Bank::Account.all(raw_acct_data)
+
+# TEST 2
+# acct_1 = Bank::Account.new(raw_acct_data[0][0], raw_acct_data[0][1].to_f)
+# Your new account id is 1212 and your balance is 1235667.0.
+
+
+# TEST 1
+# puts "Your new account id is #{acct_1.id} and your balance is #{acct_1.balance}."
+# returns appropriately is_negative': You must enter a positive balance! (ArgumentError)
+
+
+
+
+
+
+# self.all # class method # could this not be a sepreate data sanitizer class
+# 	all_accounts_info = []
+# 	CSV.foreach("path/to/file.csv") do |row|
+# 		all_accounts.push(row)
+# 		return all_accounts
+# 	end
+		
+# self.find(id) #class method, expects 2d array, or mebe hash # could this be a seperate search class
+# return find
+# end	
+
 ################################################################
 #
 # WAVE 2 - above this line
