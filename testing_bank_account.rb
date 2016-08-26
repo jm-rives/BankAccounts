d11461986a6853f2f54b0e33b808330d98cfb199
jm-rives <joanna.rives@gmail.com>
2016-08-26 14:09:34 -0700

    ADD withdraw_using_check method

--- testing_bank_account.rb
+++ testing_bank_account.rb
@@ -15,12 +15,13 @@
 # [ ] Create a CheckingAccount class which should inherit behavior from the Account class. It should include the following updated functionality:
 
 # [ ] Updated withdrawal functionality:
-# 	() Each withdrawal 'transaction' incurs a fee of $1 that is taken out of the balance. Returns the updated account balance.
-# 	() Does not allow the account to go negative. Will output a warning message and return the original un-modified balance.
-# 	() withdraw_using_check(amount): The input amount gets taken out of the account as a result of a check withdrawal. Returns the updated account balance.
-#   () Allows the account to go into overdraft up to -$10 but not any lower
-#   () The user is allowed three free check uses in one month, but any subsequent use adds a $2 transaction fee
+# 	(x) Each **cash** withdrawal 'transaction' incurs a fee of $1 that is taken out of the balance. Returns the updated account balance.
+# 	(x) Does not allow the account to go negative. Will output a warning message and return the original un-modified balance.
+# 	(x) withdraw_using_check(amount): The input amount gets taken out of the account as a result of a check withdrawal. Returns the updated account balance.
+#   (x) Allows the account to go into overdraft up to -$10 but not any lower
+#   (x) The user is allowed three free check uses in one month, but any subsequent use adds a $2 transaction fee
 #[ ] reset_checks: Resets the number of checks used to zero
+# { } build a helper method for fee that takes two parameters
 module Bank
 	
 	class Account
@@ -45,11 +46,11 @@ module Bank
 			end
 		end
 
-		def balance_to_low
-			unless @balance > 10000
-	  		 raise ArgumentError.new("Your balance must be $10 or more!")
-	  	end
-  	end
+		# def balance_to_low
+		# 	unless @balance > 10000
+	 #  		 raise ArgumentError.new("Your balance must be $10 or more!")
+	 #  	end
+  # 	end
 
 		def see_balance
 			return @balance
@@ -72,7 +73,8 @@ module Bank
 
 	class SavingsAccount < Account #should receive parent classess attributes like is_negative
 		def initialize(id, balance, open_date)
-			super
+			super # should include ref: http://rubylearning.com/satishtalim/ruby_overriding_methods.html
+			withdraw
 			check_low_balance
 		end
 
@@ -89,22 +91,72 @@ module Bank
 			else
 				@balance = @balance - money - 200
 			end
-			see_balance
+			puts see_balance
 		end
 
 		# Interest is a percentage
 		def add_interest(rate)
 			interest = @balance * rate / 100
 			@balance = interest + balance
+			return interst # puts see_balance # just return the interst not the balance  + intersest
+		end
+	 end
+
+	class CheckingAccount < Account
+		def initialize(id, balance, open_date, check_count)
+			super
 			see_balance
+			#balance_to_low # remove this is a savings account method
+			is_negative
 		end
 
-  end
+		def withdraw(money) #consider adding fee param
+			if @balance - money - 100 < 0
+				raise ArgumentError.new("Transaction Denied! Insufficient balance!")
+			else
+				@balance = @balance - money - 100
+			end
+			puts see_balance
+		end
 
+		
+		def withdraw_using_check(amount)
+			
+			if @check_count < 3
+				# so if balance - withdraw overdrafts is greater than 10 (meaning negative 10 back to zero, ArgError should NOT raise)
+				if @balance - amount < (-1000) # corrected logic for handling negative numbers
+					raise ArgumentError.new("Transaction Denied! Insufficient balance!")
+				else
+					@balance = @balance - amount
+					@check_count += 1
+					return check_count
+					return  see_balance
+				end
+			else
+				if @balance - amount - 200 < (-1000)
+					raise ArgumentError.new("Transaction Denied! Insufficient balance!")
+				else
+					@balance = @balance - amount -200
+					@check_count += 1
+					return check_count
+					return see_balance
+				end
+			end	
+		end
+
+		# def reset_checks
+		# end
+	end
 end
-
-# I'm adding this to trouble shoot git
-saver1 = Bank::SavingsAccount.new(9999, 2000, 2016)
-# puts saver1.check_low_balanrce
-puts saver1.see_balance
-puts saver1.add_interest(0.25)
+# consider using check_reset()
+# consider adding overdraft method (using twice now)
+# consider adding fee method to be called, default could be $0.00
+# BEGIN TESTS
+checking1 = Bank::CheckingAccount.new(9919, 4000, 2016)
+# . checking1.withdraw(1000)
+# . checking1.withdraw_using_check(2000) # testing check < 3, no overdraft
+# . checking1.withdraw_using_check(5100) # testing check < 3, with excess overdraft #works as expected raises argu defined at line 125
+# . checking1.withdraw_using_check(5000)	# testing check < 3, with allowed overdraft, # initial fail throwing error with -5 overdrafts because I set overdraft value to 10 (10 cents) instead of 1000 ($10)
+# . checking1.withdraw_using_check(2000) # testing check > 3, no overdraft
+# . checking1.withdraw_using_check(5100) # testing check > 3, with excess overdraft
+checking1.withdraw_using_check(5500)	# testing check > 3, with allowed overdraft
